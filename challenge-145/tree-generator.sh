9aca3ad57b271d7f8f8c59c97ecba1e29378b44e
Aftab Khan <aftabjkhan@gmail.com>
2013-12-23 20:22:23 -0500

    Added test for odd Tree Width values

--- challenge-145/tree-generator.sh
+++ challenge-145/tree-generator.sh
@@ -10,8 +10,15 @@ read tree_width body_char trunk_char
 # If trunkChar is not null, the treeWidth and bodyChar must also be not null
 if [ -n "$trunk_char" ]
 then
-	# TODO
+	if [ "$(($tree_width % 2))" -eq 1 ]
+	then
+		# TODO
+	else 
+		echo "Could not create tree! Tree Width value must be an odd integer" 2>&1
+		exit 1
+	fi
 else
-	echo "Could not create tree! Please provide a value for all 3 parameters"
+	echo "Could not create tree! Please provide a value for all 3 parameters" 2>&1
+	exit 1
 fi
 
