562deda1fe78b1df7a6af3faed371f178aeeb0bf
Aftab Khan <aftabjkhan@gmail.com>
2013-12-31 14:48:09 -0500

    Updated CalendarSync to handle multi-line inputs. Added testing script & test cases

--- challenge-123/CalendarSync.java
+++ challenge-123/CalendarSync.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
-
+/** 
+ * A Class used for determining Julian/Lunar calendar synchronicity
+ * @author Aftab Khan
+ */
 public class CalendarSync {
 
 	public static final double DAYS_PER_LUNAR_MONTH = 29.53059;
@@ -8,14 +11,26 @@ public class CalendarSync {
 
 	public static void main (String[] args){
 		Scanner scanner = new Scanner(System.in);
-		String[] inputs = scanner.nextLine().split(", ");
-		System.out.println(getSynchronizedDays(
-			Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1])));
+		String results;
+		while(scanner.hasNext()){
+			String[] inputs = scanner.nextLine().split(", ");
+			results += getSynchronizedDays(
+				Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]))) + "\n";
+		}
+		System.out.print(results);
 	}
 
+	/*
+	 * Determines if a given number of Julian calendar years and lunar calendar 
+	 * months occurs at the same time. 
+	 * @param julianYears - the number of Julian calendar years
+	 * @param lunarMonths - the number of Lunar calendar months
+	 * @return If synchronized, returns the number of days that will have elapsed 
+	 * until synchronization. If not synchronized, returns 0.
+	 */
 	private static int getSynchronizedDays (int julianYears, int lunarMonths){
 		int julianDays = (int) Math.floor(julianYears * DAYS_PER_JULIAN_YEAR);
-		int lunarDays = (int) Math.floor(lunarMonths * DAYS_PER_LUNAR_MONTH);
+		int lunarDays = (int) Math.round(lunarMonths * DAYS_PER_LUNAR_MONTH);
 		return (julianDays == lunarDays) ? lunarDays : 0;
 	}
 
