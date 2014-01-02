77009e46b9e815c89c073b8dc660534c4541fec4
Aftab Khan <aftabjkhan@gmail.com>
2014-01-01 19:00:55 -0500

    Implemented student average grade calculation and print formatting

--- challenge-136/student_management.php
+++ challenge-136/student_management.php
@@ -2,6 +2,18 @@
 
 <?php
 
-	echo ("Hello, PHP!\n");
+	$student_count = (int) strtok(fgets(STDIN), " \n\t");
+	$assignment_count = (int) strtok(" \n\t");
+	//echo ("student_count: $student_count, assignment_count: $assignment_count");
+
+	for ($i = 0; $i < $student_count; $i++) {
+    	$student_name = strtok(fgets(STDIN), " \n\t");
+    	$average = 0;
+    	for ($j = 0; $j < $assignment_count; $j++) {
+    		$current_grade = (float) strtok(" \n\t");
+    		$average += $current_grade / $assignment_count;
+    	}
+    	echo "$student_name $average\n";
+	}
 
 ?>
