05c6f3a08e198cb309f010cc0440e9ac00779c12
Aftab Khan <aftabjkhan@gmail.com>
2014-01-05 14:50:54 -0500

    Implemented greedy algorithm for fewest coin calculation.

--- challenge-119/change_calculator.php
+++ challenge-119/change_calculator.php
@@ -1,5 +1,21 @@
-#!/usr/bin/php
+#! /usr/bin/env php
 
 <?php
+
+    $cent_values = array(
+        "Quarters" => 25,
+        "Dimes" => 10,
+        "Nickels" => 5,
+        "Pennies" => 1,
+        );
+
+    $input = trim(fgets(STDIN));
+    $rem = (int) (((float) $input) * 100);
+
+    foreach ($cent_values as $key => $value) {
+        $coin_count = (int) ($rem / $value);
+        echo "$key: $coin_count\n";
+        $rem -= $value * $coin_count;
+    }
 
 ?>
