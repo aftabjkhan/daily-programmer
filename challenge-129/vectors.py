e13474febd98cbb03ebf20d89f37ab0e5428e4fb
Aftab Khan <aftabjkhan@gmail.com>
2014-01-08 16:04:36 -0500

    Implemented reading vectors, vector operations to be calculated from stdin

--- challenge-129/vectors.py
+++ challenge-129/vectors.py
@@ -3,16 +3,39 @@ import math
 
 # Aftab Khan, 2014
 
-""" Calculate the length of a vector """
+""" Calculate the length of a vector represented by a list of numbers """
 def length(a):
 	return math.sqrt(sum([x**2 for x in a]))
 
-""" Normalize a vector (make the length equal to 1)"""
+""" Normalize a vector represented by a list of numbers (make the vector length
+	equal to 1) """
 def normalize(a):
 	vlength = length(a)
 	return [x/vlength for x in a]
 
 """ Calculate the dot product of two vectors (the sum of the products of the
-	corresponding bases of two vectors) """
+	corresponding bases of two vectors) represented by two lists of numbers """
 def dot_product(a, b):
 	return sum([x*y for (x,y) in zip(a,b)])
+
+""" Print the values of the components of a vector represented by a list of 
+	numbers to up to 6 digits of precision """
+def print_vector(a):
+	for x in a: print "%.6g" % x,
+	print ""
+
+vectors = []
+# read in N vectors of various lengths
+for i in range(int(raw_input())):
+	vectors.append(map(float, raw_input().split()))
+	del vectors[i][0]
+# perform M vector operations on the vectors
+for j in range(int(raw_input())):
+	input_line = raw_input().split()
+	vector_function = input_line.pop(0)
+	if vector_function == "n":
+		print_vector(normalize(vectors[int(input_line[0])]))
+	elif vector_function == "l":
+		print "%0.6g" % length(vectors[int(input_line[0])])
+	elif vector_function == "d":
+		print "%0.6g" % dot_product(vectors[int(input_line[0])], vectors[int(input_line[1])])
