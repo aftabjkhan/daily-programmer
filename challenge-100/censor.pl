405a06a1adf63eb7483d2db4d537a0c29fd75345
Aftab Khan <aftabjkhan@gmail.com>
2014-01-13 13:52:26 -0500

    Implemented replacement of matching words in dictionary with the corresponding number of '*' characters

--- challenge-100/censor.pl
+++ challenge-100/censor.pl
@@ -10,11 +10,20 @@ while (my $input = <STDIN>){
 	#my @tokens = split(/\s+/); # splits the string at $_ by the 'space' delimeter
 	foreach (@wordlist) {
 		s/[\s\n]+$//; # removes any trailing instances of 'space' or 'newline' from $_
-		my $index_found = index($lowercase_input, lc);
-		if ($index_found != -1){
-			print "contains $_";
-		}
+		my $search_string = quotemeta(lc);
+		$input =~ s/$search_string/'*' x length($search_string)/ge;
+		#while ($input =~ m/$search_string/g) {
+			#print "Found '$&'.  Next attempt at character " . pos($input) . "Other thing: " . $-[0]. "\n";
+		#	print "Found '$&' at: " . $-[0]. "\n";
+		#}
+		#my $index_found = index($lowercase_input, lc);
+		#if ($index_found != -1){
+
+		#	$input =~ 
+		#	print "contains $_";
+		#}
 	}
+	print "$input";
 }
 
 close $wordfile or die "$wordfile: $!";
