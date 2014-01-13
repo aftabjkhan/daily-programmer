cde775628c98f5a4fc2455f2335e2a4fffb2de53
Aftab Khan <aftabjkhan@gmail.com>
2014-01-13 14:13:54 -0500

    Cleaned up code, added comments for clarity. Added entries to dictionary

--- challenge-100/censor.pl
+++ challenge-100/censor.pl
@@ -2,26 +2,15 @@
 use strict;
 use warnings;
 
-open(my $wordfile,  "<",  $ARGV[0]) or die "Can't open file at ARGV[0] $ARGV[0]: $!";
+open(my $wordfile,  "<",  $ARGV[0]) or die "Can't open file $ARGV[0]: $!";
 my @wordlist = <$wordfile>;
 
 while (my $input = <STDIN>){
 	my $lowercase_input = lc($input);
-	#my @tokens = split(/\s+/); # splits the string at $_ by the 'space' delimeter
 	foreach (@wordlist) {
-		s/[\s\n]+$//; # removes any trailing instances of 'space' or 'newline' from $_
-		my $search_string = quotemeta(lc);
-		$input =~ s/$search_string/'*' x length($search_string)/ge;
-		#while ($input =~ m/$search_string/g) {
-			#print "Found '$&'.  Next attempt at character " . pos($input) . "Other thing: " . $-[0]. "\n";
-		#	print "Found '$&' at: " . $-[0]. "\n";
-		#}
-		#my $index_found = index($lowercase_input, lc);
-		#if ($index_found != -1){
-
-		#	$input =~ 
-		#	print "contains $_";
-		#}
+		s/[\s\n]+$//;  # removes trailing instances of 'space' or 'newline' from $_
+		my $word = quotemeta(lc);  # sets $word to lowercase, escaped version of $_
+		$input =~ s/$word/'*' x length($word)/ge;
 	}
 	print "$input";
 }
