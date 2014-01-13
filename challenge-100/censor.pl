bc0928916e50652572e4b92e9529479b1215360a
Aftab Khan <aftabjkhan@gmail.com>
2014-01-13 12:37:45 -0500

    Implemented substring instance location in input from dictionary file (passed as arg). Added badword.txt dictionary (taken from url in challenge description)

--- challenge-100/censor.pl
+++ challenge-100/censor.pl
@@ -1,3 +1,20 @@
 #!/usr/bin/env perl
- use strict;
- use warnings;
+use strict;
+use warnings;
+
+open(my $wordfile,  "<",  $ARGV[0]) or die "Can't open file at ARGV[0] $ARGV[0]: $!";
+my @wordlist = <$wordfile>;
+
+while (my $input = <STDIN>){
+	my $lowercase_input = lc($input);
+	#my @tokens = split(/\s+/); # splits the string at $_ by the 'space' delimeter
+	foreach (@wordlist) {
+		s/[\s\n]+$//; # removes any trailing instances of 'space' or 'newline' from $_
+		my $index_found = index($lowercase_input, lc);
+		if ($index_found != -1){
+			print "contains $_";
+		}
+	}
+}
+
+close $wordfile or die "$wordfile: $!";
