f4b7fcaffcf093b760c19f37511a964252c6e6ed
Aftab Khan <aftabjkhan@gmail.com>
2014-01-14 02:18:22 -0500

    Changed functionality/implementation to preserve printing the first letter of censored portions of the input. Added tests, testconfig

--- challenge-100/censor.pl
+++ challenge-100/censor.pl
@@ -5,13 +5,18 @@ use warnings;
 open(my $wordfile,  "<",  $ARGV[0]) or die "Can't open file $ARGV[0]: $!";
 my @wordlist = <$wordfile>;
 
-while (my $input = <STDIN>){
-	my $lowercase_input = lc($input);
+while (my $input = <STDIN>){	#read file line by line to $input
+	
 	foreach (@wordlist) {
-		s/[\s\n]+$//;  # removes trailing instances of 'space' or 'newline' from $_
-		my $word = quotemeta(lc);  # sets $word to lowercase, escaped version of $_
-		$input =~ s/$word/'*' x length($word)/ge;
+		s/[\s\n]+$//;  # removes trailing 'space' or 'newline' instances from $_
+
+		while ($input =~ m/\Q$_/gi) { # ignore case, match all (escaped) $_ in input
+			my $censor_length = length($_) - 1;
+			# replace '*'s at match start posn ($-[0]) + 1 to preserve first letter
+			substr $input, $-[0]+1, $censor_length, ('*' x $censor_length);
+		}
 	}
+
 	print "$input";
 }
 
